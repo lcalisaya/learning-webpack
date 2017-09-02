bee0f212b43eea843d0c802be6ddf35838374fae
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-09-02 12:22:09 -0300

    Loaders style, css y scss para incluir estilos y compilar sass

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,18 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'hello.bundle.js'
   },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [ "style-loader", "css-loader", "sass-loader" ]
+    }]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Demo! Lalal',
-      minify: {
-        collapseWhitespace: true
-      },
+      // minify: {
+      //   collapseWhitespace: true
+      // },
       hash: true,
       template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
     })
